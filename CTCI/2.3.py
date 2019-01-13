d84b77521b901acb75bf275baf9844ee930ee505
Aditya Narayanan <adityanarayanan.narayanan@gmail.com>
2019-01-13 11:48:44 -0700

    Add files via upload

---
+++ CTCI/2.3.py
@@ -0,0 +1,16 @@
+# Q) 2.3: Implement an algorithm to delete a node in the middle of a single linked list, given only access to that node
+#code
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
