69c3c8b1254774f1c38e1618162b9db98ea36209
Aditya Narayanan <adityanarayanan.narayanan@gmail.com>
2019-02-16 13:03:33 -0700

    Create 4.3.java

---
+++ CTCI/4.3.java
@@ -0,0 +1,53 @@
+                            // Q.) Given a sorted (increasing order) array, write an algorithm to create a binary tree 
+                            //     with minimal height.
+    
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public int maxDepth(TreeNode root){
+            if(root == null){
+                return 0;
+            }
+            else{
+                return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
+            }
+
+        }
+        public boolean checker(TreeNode root){
+            if(root == null){
+                return true;
+            }else{
+                if(Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1 ){
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+        }
+        boolean flag = true;
+        public boolean inOrder(TreeNode root){
+            if(root!=null){
+                if(checker(root) == false){
+                    flag = false;
+                    return flag;
+                }else{
+                    inOrder(root.left);
+                    inOrder(root.right);
+                }
+            }
+            return flag;
+        }
+        public boolean isBalanced(TreeNode root) {
+            //System.out.println(maxDepth(root.right));
+            return inOrder(root);
+
+
+        }
+    }
